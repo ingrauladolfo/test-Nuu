0cf3512674b3bfec1b72811b93dbb1c59fa119c8
Ing. Raúl Adolfo Torres Vargas <ingrauladolfo.torresvargas@gmail.com>
2021-11-22 16:52:42 -0600

    first commit

---
+++ js/drag.js
@@ -0,0 +1,29 @@
+var elem_destino, elem_origen;
+function comenzar(){
+    elem_origen = document.getElementById("imagen");
+    elem_origen.addEventListener("dragstart",comenzamos_arrastrar, false);
+    elem_destino = document.getElementById("zonadestino");
+    elem_destino.addEventListener("dragenter",entrando,false);
+    elem_destino.addEventListener("dragover", function(e){
+        e.preventDefault();}, false);
+    elem_destino.addEventListener("drop", soltado,false);
+    elem_origen.addEventListener("dragend", terminado,false);
+
+}
+function comenzamos_arrastrar(e){
+    var codigo = "<img src='"+ elem_origen.getAttribute("src")+"'>";
+    e.dataTransfer.setData("Text", codigo);
+}
+function soltado(e){
+    e.preventDefault();
+    elem_destino.innerHTML = e.dataTransfer.getData("Text");
+}
+function terminado(e){
+    var elemento = e.target;
+    elemento.style.visibility = "hidden";
+}
+function entrando(e){
+    e.preventDefault();
+    elem_destino.style.background = "rgba(8,252,25,0.8)";
+}
+window.addEventListener("load",comenzar,false);
